0068b951d789133377e6b49c3930b6e164512599
Ranga Senal Fernando <ranga.fernando@gmail.com>
2023-03-21 09:04:37 +1100

    Implement Producer and Consumer

---
+++ Weather.Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ConsumerConfig
+{
+    GroupId = "weather-consumer-group",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+var consumer = new ConsumerBuilder<Null, string>(config).Build();
+
+consumer.Subscribe("weather-topic");
+
+CancellationTokenSource tokenSource = new();
+
+try
+{
+    while (true)
+    {
+        var response = consumer.Consume(tokenSource.Token);
+        if(response.Message != null)
+        {
+            var weather = JsonConvert.DeserializeObject<Weather>(response.Message!.Value);
+            Console.WriteLine($"Stae: {weather!.State}, - Temp: {weather.Temarature}");
+        }
+    }
+}
+catch (System.Exception)
+{
+    
+    throw;
+}
+
+public record Weather(string State, int Temarature);
---
+++ Weather.Producer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ProducerConfig 
+                { 
+                    BootstrapServers = "localhost:9092"
+                };
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+    string? state;
+    while ((state = Console.ReadLine()) != null)
+    {
+        var response = producer.ProduceAsync("weather-topic", 
+            new Message<Null, string> 
+            { 
+                Value = JsonConvert.SerializeObject(new Weather(state, 70))
+            });    
+    }
+}
+catch (ProduceException<Null, string> ex)
+{
+    Console.Write(ex.Message);    
+}
+
+public record Weather(string State, int Temarature);
